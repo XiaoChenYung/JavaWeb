64e7632be37798090dbd0b4be5b6a262f4e1d2a8
杨晓晨 <xiaochenyung@gmail.com>
2016-08-23 18:17:40 +0800

    servlet

---
+++ src/javademo/ServletDemo.java
@@ -0,0 +1,26 @@
+package javademo;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by tm on 16/8/23.
+ */
+@WebServlet(name = "ServletDemo")
+public class ServletDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.write("<h1> hello first servlet!</h1>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.write("<h1> hello first servlet!</h1>");
+    }
+}
