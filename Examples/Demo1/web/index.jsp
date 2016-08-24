34c46a0dce7bfed5b242e9d598e2f6784828a130
杨晓晨 <xiaochenyung@gmail.com>
2016-08-24 10:47:45 +0800

    servlet 测试成功

--- Examples/Demo1/web/index.jsp
+++ Examples/Demo1/web/index.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: tm
-  Date: 16/8/23
-  Time: 下午4:27
+  Date: 16/8/24
+  Time: 上午10:10
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>哈哈哈</title>
+    <title>测试</title>
   </head>
   <body>
-Hello world yxc ennnenadfhlasdf
+  哈哈哈哈,OK asdhfljkasdhflkasdhflas
   </body>
 </html>
--- src/javademo/ServletDemo.java
+++ src/javademo/ServletDemo.java
@@ -14,13 +14,33 @@ import java.io.IOException;
 public class ServletDemo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter pw = response.getWriter();
-        pw.write("<h1> hello first servlet!</h1>");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("    This is ");
+        out.print(this.getClass());
+        out.println(", using the POST method");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter pw = response.getWriter();
-        pw.write("<h1> hello first servlet!</h1>");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("    This is ");
+        out.print(this.getClass());
+        out.println(", using the POST method");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
     }
 }
