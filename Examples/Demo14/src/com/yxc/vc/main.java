e8cce670e1bc6e95dacec69a2f37a5f5715b859f
杨晓晨 <xiaochenyung@gmail.com>
2016-08-31 11:25:45 +0800

    delete

--- Examples/Demo14/src/com/yxc/vc/main.java
+++
@@ -1,8 +0,0 @@
-package com.yxc.vc;
-
-/**
- * Created by tm on 16/8/29.
- */
-public class main {
-
-}
--- Examples/Demo2/src/Demo0/Spring.java
+++
@@ -1,7 +0,0 @@
-package Demo0;
-
-/**
- * Created by tm on 16/8/25.
- */
-public class Spring {
-}
